248a055ebddad8a6b69991ebead362cb8e18a714
Mauricio Fow <mauriciofow@gmail.com>
2024-08-01 23:52:54 +0300

    easy interface

--- cmd/main.go
+++ cmd/main.go
@@ -8,12 +8,13 @@ import (
 )
 
 func main() {
-	video2 := video.ImageToVideo("assets/jojos.jpg", 2)
+	video2 := video.ImageToVideo("assets/jojos.jpg", 2, 400, 400)
 
-	video1 := video.ImageToVideo("assets/jojos.jpg", 3)
+	video1 := video.ImageToVideo("assets/jojos.jpg", 3, 400, 400)
+
+	video1.VFlip()
 
 	custom := video.MergeVideos(&video1, &video2)
 
 	custom.SaveVideo(fmt.Sprintf("test%d.mp4", rand.Intn(100)))
-
 }
--- internal/video/video.go
+++ internal/video/video.go
@@ -8,47 +8,54 @@ import (
 	ffmpeg "github.com/u2takey/ffmpeg-go"
 )
 
-type CustomStream struct {
-	Stream *ffmpeg.Stream
+type Video struct {
+	stream *ffmpeg.Stream
 }
 
 func ReadVideo(path string) *ffmpeg.Stream {
 	return ffmpeg.Input(path)
 }
 
-func ImageToVideo(path string, duration int) CustomStream {
-	c := CustomStream{}
+func ImageToVideo(path string, duration int, width int, height int) Video {
+	c := Video{}
 	if duration < 0 {
 		panic("duration can't be less than 0")
 	}
 
-	c.Stream = ffmpeg.Input(path, ffmpeg.KwArgs{"loop": 1, "t": duration, "framerate": 1}).Crop(0, 0, 500, 500).Filter("scale", ffmpeg.Args{"500:500"})
+	c.stream = ffmpeg.
+		Input(path, ffmpeg.KwArgs{"loop": 1, "t": duration, "framerate": 1}).
+		Crop(0, 0, width, height).
+		Filter("scale", ffmpeg.Args{fmt.Sprintf("%d:%d", width, height)})
 
 	return c
 
 }
 
-func (s *CustomStream) GetVideoOutput(name string) *CustomStream {
-	s.Stream = s.Stream.Output(fmt.Sprintf("%s", name), ffmpeg.KwArgs{"c:v": "libx264", "r": 25, "framerate": 1})
+func (s *Video) GetVideoOutput(name string) *Video {
+	s.stream = s.stream.Output(fmt.Sprintf("%s", name), ffmpeg.KwArgs{"c:v": "libx264", "r": 25, "framerate": 1})
 
 	return s
 }
 
-func (s *CustomStream) SaveVideo(name string) {
+func (s *Video) SaveVideo(name string) {
 	buf := bytes.NewBuffer(nil)
 
-	s.GetVideoOutput(name).Stream.WithOutput(buf, os.Stdout).Run()
+	s.GetVideoOutput(name).stream.WithOutput(buf, os.Stdout).Run()
 
 }
 
-func MergeVideos(streams ...*CustomStream) CustomStream {
-	merged := CustomStream{}
+func MergeVideos(streams ...*Video) Video {
+	merged := Video{}
 	videos := []*ffmpeg.Stream{}
 
 	for _, v := range streams {
-		videos = append(videos, v.Stream)
+		videos = append(videos, v.stream)
 	}
-	merged.Stream = ffmpeg.Concat(videos)
+	merged.stream = ffmpeg.Concat(videos)
 
 	return merged
+}
+
+func (s *Video) VFlip() {
+	s.stream = s.stream.VFlip()
 }
