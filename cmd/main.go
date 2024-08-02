f2314b855c080586cb6b634373f950630e8865bb
Mauricio Fow <mauriciofow@gmail.com>
2024-08-02 18:58:55 +0300

    improved structs

--- cmd/main.go
+++ cmd/main.go
@@ -2,28 +2,29 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 
 	"github.com/thedekerone/gobra/internal/video"
 )
 
 func main() {
 
-	video2 := video.ImageToVideo("assets/story1.jpg", 4)
+	video2 := video.ImageToVideo("assets/story1.jpg", 7, 30)
 
-	video1 := video.ImageToVideo("assets/story2.jpg", 4)
+	video1 := video.ImageToVideo("assets/story2.jpg", 7, 30)
 
-	video3 := video.ImageToVideo("assets/story3.jpg", 4)
+	video3 := video.ImageToVideo("assets/story3.jpg", 7, 30)
 
-	video4 := video.ImageToVideo("assets/story4.jpg", 4)
+	video4 := video.ImageToVideo("assets/story4.jpg", 7, 30)
 
 	videos := []*video.Video{&video1, &video2, &video3, &video4}
 
 	for i, _ := range videos {
 		videos[i] = videos[i].Crop(900, 900)
+		videos[i] = videos[i].AddFadeIn(1)
+		videos[i] = videos[i].AddFadeOut(1)
 	}
 
 	custom := video.MergeVideos(&video1, &video2, &video3, &video4)
 
-	custom.SaveVideo(fmt.Sprintf("test%d.mp4", rand.Intn(100)))
+	custom.SaveVideo(fmt.Sprintf("test.mp4"))
 }
--- internal/video/video.go
+++ internal/video/video.go
@@ -9,28 +9,31 @@ import (
 )
 
 type Video struct {
-	stream *ffmpeg.Stream
+	stream   *ffmpeg.Stream
+	fps      int
+	duration int
 }
 
 func ReadVideo(path string) *ffmpeg.Stream {
 	return ffmpeg.Input(path)
 }
 
-func ImageToVideo(path string, duration int) Video {
+func ImageToVideo(path string, duration int, fps int) Video {
 	c := Video{}
 	if duration < 0 {
 		panic("duration can't be less than 0")
 	}
 
-	c.stream = ffmpeg.
-		Input(path, ffmpeg.KwArgs{"loop": 1, "t": duration, "framerate": 1})
+	c.duration = duration
+	c.fps = fps
+	c.stream = ffmpeg.Input(path, ffmpeg.KwArgs{"loop": 1, "t": duration, "framerate": 30})
 
 	return c
 
 }
 
 func (s *Video) GetVideoOutput(name string) *Video {
-	s.stream = s.stream.Output(fmt.Sprintf("%s", name), ffmpeg.KwArgs{"c:v": "libx264", "r": 25, "framerate": 1})
+	s.stream = s.stream.Output(fmt.Sprintf("%s", name), ffmpeg.KwArgs{"c:v": "libx264", "r": s.fps, "framerate": 1})
 
 	return s
 }
@@ -45,12 +48,15 @@ func (s *Video) SaveVideo(name string) {
 func MergeVideos(streams ...*Video) Video {
 	merged := Video{}
 	videos := []*ffmpeg.Stream{}
+	newDuration := 0
 
 	for _, v := range streams {
 		videos = append(videos, v.stream)
+		newDuration += v.duration
 	}
 	merged.stream = ffmpeg.Concat(videos)
-
+	merged.fps = streams[0].fps
+	merged.duration = newDuration
 	return merged
 }
 
@@ -87,5 +93,35 @@ func (s *Video) Input(path string, kwargs ffmpeg.KwArgs) *Video {
 
 func (s *Video) Output(name string, kwargs ffmpeg.KwArgs) *Video {
 	s.stream = s.stream.Output(name, kwargs)
+	return s
+}
+
+func (s *Video) AddFadeIn(duration int) *Video {
+	if duration < 0 {
+		panic("duration can't be less than 0")
+	}
+	s.stream = s.stream.
+		Filter("fade", ffmpeg.Args{"t=in", fmt.Sprintf("d=%d", duration)})
+
+	return s
+}
+
+func (s *Video) AddFadeOut(duration int) *Video {
+	if duration < 0 {
+		panic("duration can't be less than 0")
+	}
+	s.stream = s.stream.
+		Filter("fade", ffmpeg.Args{"t=out", fmt.Sprintf("d=%d", duration), fmt.Sprintf("st=%d", s.duration-duration)})
+
+	return s
+}
+
+func (s *Video) AddZoomIn(start int, duration int) *Video {
+	if duration < 0 {
+		panic("duration can't be less than 0")
+	}
+	s.stream = s.stream.
+		Filter("zoompan", ffmpeg.Args{"z=min(max(zoom,pzoom)+0.001,1.25)", "d=1", "fps=60", "s=1800x1800"})
+
 	return s
 }
